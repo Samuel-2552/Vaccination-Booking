7db0a54d8a08dbfa1bfd5659a42571f857f280c9
Samuel-2552 <104893913+Samuel-2552@users.noreply.github.com>
2023-05-18 14:19:29 +0000

    modified databse to include GPS coordinates

--- python-code/database.py
+++ python-code/database.py
@@ -1,44 +1,46 @@
 import sqlite3
 
-# Create a connection to the SQLite database
+#Create a connection to the SQLite database
 conn = sqlite3.connect('vaccination_app.db')
 cursor = conn.cursor()
 
-# Create User table
+#Create User table
 create_user_table_query = '''
 CREATE TABLE IF NOT EXISTS User (
-    id INTEGER PRIMARY KEY,
-    username TEXT NOT NULL,
-    email TEXT NOT NULL,
-    password TEXT NOT NULL
+id INTEGER PRIMARY KEY,
+username TEXT NOT NULL,
+email TEXT NOT NULL,
+password TEXT NOT NULL
 );
 '''
 cursor.execute(create_user_table_query)
 
-# Create VaccinationCenter table
+#Create VaccinationCenter table
 create_vaccination_center_table_query = '''
 CREATE TABLE IF NOT EXISTS VaccinationCenter (
-    id INTEGER PRIMARY KEY,
-    name TEXT NOT NULL,
-    working_hours TEXT NOT NULL
+id INTEGER PRIMARY KEY,
+name TEXT NOT NULL,
+working_hours TEXT NOT NULL,
+gps_location TEXT NOT NULL
 );
 '''
 cursor.execute(create_vaccination_center_table_query)
 
-# Create Slot table
+#Create Slot table
 create_slot_table_query = '''
 CREATE TABLE IF NOT EXISTS Slot (
-    id INTEGER PRIMARY KEY,
-    center_id INTEGER NOT NULL,
-    date TEXT NOT NULL,
-    capacity INTEGER NOT NULL,
-    available_slots INTEGER NOT NULL,
-    user_names TEXT,
-    FOREIGN KEY (center_id) REFERENCES VaccinationCenter(id)
+id INTEGER PRIMARY KEY,
+center_id INTEGER NOT NULL,
+date TEXT NOT NULL,
+capacity INTEGER NOT NULL,
+available_slots INTEGER NOT NULL,
+user_id INTEGER,
+FOREIGN KEY (center_id) REFERENCES VaccinationCenter(id),
+FOREIGN KEY (user_id) REFERENCES User(id)
 );
 '''
 cursor.execute(create_slot_table_query)
 
-# Commit the changes and close the connection
+#Commit the changes and close the connection
 conn.commit()
 conn.close()
