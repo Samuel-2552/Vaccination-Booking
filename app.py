541e216f9bdfc8ec8e7612e5a490c144936e6019
Samuel-2552 <104893913+Samuel-2552@users.noreply.github.com>
2023-06-13 11:41:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def g_mail(to_email,subject,body):
 
     # Sender's email and password
     gmail_user = "201501502@rajalakshmi.edu.in"
-    gmail_password = "Automate@Agra$2552"
+    gmail_password = "#"
 
 
     # Prepare the email
